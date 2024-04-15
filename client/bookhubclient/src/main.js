15d23ff0d684d972c976c31896269a80036dbb87
fenglan12 <2867423475@qq.com>
2024-04-15 19:54:47 +0800

    create client project

--- client/bookhubclient/src/main.js
+++ client/bookhubclient/src/main.js
@@ -1,5 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
