ab2fed37e6aba4f7a3026e12caf2945f72a09353
Kettkk <a962498793@gmail.com>
2024-04-21 18:43:14 +0800

    create buyGoodsView and routers

---
+++ client/bookhubclient/src/views/tradeView/buyGoodsView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import NavHeaderComponent from "@/components/homeViewComoponents/navHeaderComponent.vue";
+import HeaderSearchBoxComponent from "@/components/homeViewComoponents/headerSearchBoxComponent.vue";
+</script>
+
+<template>
+  <div>
+    <el-header>
+      <nav-header-component></nav-header-component>
+      <header-search-box-component></header-search-box-component>
+    </el-header>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
