ac3834b93d76513255d9fbe0033c9f222b98ebeb
Kettkk <a962498793@gmail.com>
2024-04-22 20:40:16 +0800

    create goodImgUploader component and update publishGoodsViewMainComponent

--- client/bookhubclient/src/components/tradeViewComponents/publishGoodsViewMainComponent.vue
+++ client/bookhubclient/src/components/tradeViewComponents/publishGoodsViewMainComponent.vue
@@ -1,34 +1,10 @@
 <script lang="ts" setup>
-const username = 'Jay';
 import { ref } from 'vue'
-import { ElMessage } from 'element-plus'
-import { Plus } from '@element-plus/icons-vue'
-
-import type { UploadProps } from 'element-plus'
 
 const price = ref('')
 
 const goodName = ref('')
 
-const imageUrl = ref('')
-
-
-const handleAvatarSuccess: UploadProps['onSuccess'] = (
-    response,
-    uploadFile
-) => {
-  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
-}
-const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-  if (rawFile.type !== 'image/jpeg') {
-    ElMessage.error('Avatar picture must be JPG format!')
-    return false
-  } else if (rawFile.size / 1024 / 1024 > 2) {
-    ElMessage.error('Avatar picture size can not exceed 2MB!')
-    return false
-  }
-  return true
-}
 </script>
 
 <template>
@@ -37,16 +13,7 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
   <el-container>
     <el-aside>
       <el-container id="goodImgContainer">
-        <el-upload
-            class="avatar-uploader"
-            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-            :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            :before-upload="beforeAvatarUpload"
-        >
-          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-        </el-upload>
+
       </el-container>
 
       <div style="width: 300px;height: 100px;">
