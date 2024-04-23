0b7aa5987b01b45f46adc6e058f86c105be1672d
Kettkk <a962498793@gmail.com>
2024-04-23 23:13:58 +0800

    debug Finish.

--- server/bookHubServer/bookHubServer/obj/Debug/net7.0/bookHubServer.AssemblyInfo.cs
+++ server/bookHubServer/bookHubServer/obj/Debug/net7.0/bookHubServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookHubServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c00d371e5fe7df30895ff421f560cec1491457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookHubServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookHubServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
