fb0ba5193e1c20fcf210d406e821bcab350681bd
“JasonTang39” <JasonTang39@outlook.com>
2024-05-16 16:55:18 +0800

    complete search function

--- client/bookhubclient/src/components/homeViewComoponents/headerSearchBoxComponent.vue
+++ client/bookhubclient/src/components/homeViewComoponents/headerSearchBoxComponent.vue
@@ -1,54 +1,85 @@
 <!--搜索栏开始-->
 <script setup>
-    import { ref } from 'vue'
-    import { Search } from '@element-plus/icons-vue'
-    import router from "@/router/index.js";
-    const input3 = ref('')
-    const go2home = () => {
-        if (router.currentRoute.value.name === 'home') {
-            window.location.reload();
-        } else {
-            router.push({ name: 'home' });
-        }
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+import router from "@/router/index.js";
+const input3 = ref('')
+const go2home = () => {
+    if (router.currentRoute.value.name === 'home') {
+        window.location.reload();
+    } else {
+        router.push({ name: 'home' });
     }
+}
+
+
+const searchBook = () => {
+    if (input3.value != '')
+    {
+        if(router.currentRoute.value.path != '/searchBooks')
+        {
+            router.replace({
+                path: '/searchBooks',
+                query: {
+                    keyword:input3.value
+                }
+            });
+        }else{            
+            router.replace({
+                query:{
+                    keyword:input3.value
+                }
+            })
+        } 
+    }
+       
+}
+
+
 </script>
 <template>
     <div id="headerSearchBox" class="header-search-box">
         <div class="header-search">
-            <img src="@/assets/HDUBookhub.png" class="logo" alt="logo" @click = "go2home"/>
-                <div class="search-box">
-                    <div class="mt-4">
-                        <el-input v-model="input3" style="max-width: 600px" placeholder="商品名称" class="input-with-select" size="large">
-                            <template #append>
-                                <el-button :icon="Search" />
-                            </template>
-                        </el-input>
-                    </div>
+            <img src="@/assets/HDUBookhub.png" class="logo" alt="logo" @click="go2home" />
+            <div class="search-box">
+                <div class="mt-4">
+                    <el-input v-model="input3" style="max-width: 600px" placeholder="商品名称" class="input-with-select"
+                        size="large" @keydown.enter="searchBook">
+                        <template #append>
+                            <el-button :icon="Search" @click="searchBook" />
+                        </template>
+                    </el-input>
                 </div>
+            </div>
         </div>
     </div>
 </template>
+
 <style>
-    .header-search-box {
-        background: #f2f1ea;
-        height: 100px;
-    }
-    .header-search{
-        width: 1200px;
-        height: 100px;
-        margin: 0 auto;
-        padding-top: 25px;
-        box-sizing: border-box;
-    }
-    .logo {
-        max-height: 40px;
-        cursor: pointer;
-    }
-    .search-box {
-        float: right;
-    }
-    .mt-4 {
-        margin-top: 4px;
-        width: 600px;
-    }
+.header-search-box {
+    background: #f2f1ea;
+    height: 100px;
+}
+
+.header-search {
+    width: 1200px;
+    height: 100px;
+    margin: 0 auto;
+    padding-top: 25px;
+    box-sizing: border-box;
+}
+
+.logo {
+    max-height: 40px;
+    cursor: pointer;
+}
+
+.search-box {
+    float: right;
+}
+
+.mt-4 {
+    margin-top: 4px;
+    width: 600px;
+}
 </style>
---
+++ client/bookhubclient/src/components/searchBooksViewComponents/searchedBookList.vue
@@ -0,0 +1,70 @@
+<script setup>
+import GoodsWindow from "@/components/otherAndYourProfileComponents/child3ViewComponents/goodsWindow.vue";
+import axios from "axios";
+import { ref ,computed} from 'vue'
+const goodsPerPage = 12; // 每页显示的商品数量
+const currentPage = ref(1); // 当前页码
+const route = useRoute();
+import { useRoute } from 'vue-router';
+const goodsList = ref([])
+const keyWord = ref('') 
+
+keyWord.value = route.query.keyword
+
+const url ="http://localhost:5062/api/Search?keyword="+encodeURIComponent(keyWord.value)
+
+axios.post(url)
+  .then(function (response) {
+    goodsList.value=response.data
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+const paginatedGoods = computed(() => {
+    const start = (currentPage.value - 1) * goodsPerPage;
+    const end = start + goodsPerPage;
+    return goodsList.value.slice(start, end);
+});
+
+function handleCurrentChange(val) {
+    currentPage.value = val;
+}
+</script>
+
+
+
+<template>
+    <div>
+        <div id="goodsWindowsContainer">
+            <GoodsWindow v-for="good in paginatedGoods" :key="good.id" :good="good"></GoodsWindow>
+        </div>
+        <div id="scrollable-container">
+            <el-pagination class="pagination" background layout="prev, pager, next" :total="goodsList.length"
+                :page-size="goodsPerPage" @current-change="handleCurrentChange"></el-pagination>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#goodsWindowsContainer {
+  background-color: #f6f8fa;
+  width: 100%;
+  height: 650px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#scrollable-container {
+  width: 100%;
+  height: 50px;
+  margin-top: 50px;
+  margin-left: 50px;
+}
+
+.pagination {
+  margin-top: 10px;
+}
+</style>
